f11efed298d39db9b0928d60b62c8509e4d1512a
Jesse <jessexinyu@foxmail.com>
2022-03-27 17:48:52 +0800

    queue

---
+++ examples/data-structure/queue.js
@@ -0,0 +1,13 @@
+import Queue from '../../dist/data-structure/Queue.js'
+
+const queue = new Queue()
+console.log(queue.isEmpty())
+// queue.enqueue(5)
+// queue.enqueue(8)
+// // console.log(stack)
+// // console.log(stack.count)
+// queue.dequeue()
+// console.log(queue.size())
+// console.log(queue)
+// // queue.clear()
+// console.log(queue.toString())
