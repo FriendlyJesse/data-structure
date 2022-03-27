1970d8301a2ab188683a1edae3642c5dcd7aa53e
Jesse <jessexinyu@foxmail.com>
2022-03-27 17:09:51 +0800

    stack

---
+++ examples/data-structure/Stack.js
@@ -0,0 +1,9 @@
+import Stack from '../../dist/data-structure/Stack.js'
+
+const stack = new Stack()
+stack.push(5)
+stack.push(8)
+// console.log(stack)
+// console.log(stack.count)
+
+console.log(stack.toString())
